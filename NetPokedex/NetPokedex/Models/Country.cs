db2c386b58f7bfb4e309eb7ca28d78e928bdd619
nhlong1512 <20521569@gm.uit.edu.vn>
2023-07-14 17:26:07 +0700

    push init

---
+++ NetPokedex/NetPokedex/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace NetPokedex.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
---
+++ NetPokedex/NetPokedex/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace NetPokedex.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public String Text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
+    }
+}
